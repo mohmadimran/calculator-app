0e79693aae07a5898944277e46c330412dcb3956
mohmadimran <pathanimrankhan308@gmail.com>
2025-01-13 23:35:03 +0530

    add all file of calculator app

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+export default function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (val) => {
+    if (val === "=") {
+      try {
+        if (!input.trim()) {
+          setResult("Error");
+        } else {
+          const evalResult = eval(input);
+          setResult(isNaN(evalResult) ? "NaN" : evalResult);
+        }
+      } catch (error) {
+        setInput(error);
+      }
+    } else if (val === "c") {
+      setInput("");
+      setResult("");
+    } else {
+      setInput((previous) => previous + val);
+    }
+  };
+  return (
+    <div className="app">
+      <div className="calculator-container">
+        <h1>React Calculator</h1>
+        <input type="text" value={input} readOnly />
+        <div>{result}</div>
+        <div className="calculator-btn-box">
+          {[
+            "7",
+            "8",
+            "9",
+            "+",
+            "4",
+            "5",
+            "6",
+            "-",
+            "1",
+            "2",
+            "3",
+            "*",
+            "c",
+            "0",
+            "=",
+            "/",
+          ].map((val) => {
+            return (
+              <button
+                className="btn"
+                key={val}
+                onClick={() => handleClick(val)}
+              >
+                {val}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
